2d5813fca49bdf7cb03dcdc6f8f97beb9769f662
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-07-23 22:32:03 +0300

    Added bootstrap styling for better visual appeal

--- Github-search/src/app/services/user.service.ts
+++ Github-search/src/app/services/user.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
-
+import { map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private username:string;
+
+  constructor(private http:HttpClient) {
+   console.log("Service is now ready");
+   this.username = 'wadi-1000';
+
+   }
+   getProfileInfo(){
+     return this.http.get("https://api.github.com/users/" + this.username )
+    map((res: { json: () => any; }) => res.json());   }
 }
