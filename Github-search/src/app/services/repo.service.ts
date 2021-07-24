756ec2084018ed73ed5ace194873a8810b7349a5
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-07-24 12:32:06 +0300

    Added my list of repositories

--- Github-search/src/app/services/repo.service.ts
+++ Github-search/src/app/services/repo.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepoService {
+  
+  private repos:[];
 
-  constructor() { }
+  constructor(private http:HttpClient) {
+   console.log("Service is now ready");
+   this.repos = [];
+
+  }
+
+  getProfileRepos(){
+    return this.http.get("https://api.github.com/users/wadi-1000/repos")
+   map((res: { json: () => any; }) => res.json());   }
 }
