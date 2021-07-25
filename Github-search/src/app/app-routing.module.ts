6bc988fdd7599376cf806bd5d793073aaaac9de7
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-07-25 18:56:24 +0300

    Implemented the not found pages

--- Github-search/src/app/app-routing.module.ts
+++ Github-search/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
 import { SearchFormComponent } from './search-form/search-form.component';
-
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'user', component: UserComponent},
+  { path:'**', component:NotFoundComponent},
+
+  { path: '', redirectTo:"/user", pathMatch:"full"},
 
 ];
 
